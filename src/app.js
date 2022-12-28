c5020fdf28acd51da2bb0eb0140033112f217ae0
Carlos Herrera <c.herrerav97@gmail.com>
2022-12-28 00:46:59 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My granma", "The turtle", "My bird", "My hamster"];
+  let what = ["eat", "pissed", "crushed", "broked", "stole"];
+  let object = ["my computer", "my phone", "my homework", "my clothes"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const whoRandom = Math.floor(Math.random() * who.length);
+  const whatRandom = Math.floor(Math.random() * what.length);
+  const objectRandom = Math.floor(Math.random() * object.length);
+  const whenRandom = Math.floor(Math.random() * when.length);
+
+  function excuseGenerator(whoIndex, whatIndex, objectIndex, whenIndex) {
+    let excuseResult = `${who[whoIndex]} ${what[whatIndex]} ${object[objectIndex]} ${when[whenIndex]}.`;
+    return excuseResult;
+  }
+
+  console.log(excuseGenerator(whoRandom, whatRandom, objectRandom, whenRandom)); //Solo para verificar en la terminal
+
+  const excuseElement = document.querySelector("#excuse");
+  excuseElement.innerText = excuseGenerator(
+    whoRandom,
+    whatRandom,
+    objectRandom,
+    whenRandom
+  );
 };
